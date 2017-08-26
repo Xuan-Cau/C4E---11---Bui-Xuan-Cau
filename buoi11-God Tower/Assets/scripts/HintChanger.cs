ad2cbfc1f503b84f2f1efed19f5fdd5eed9e8aea
PC\Admin <bxcau97@gmail.com>
2017-08-26 22:41:26 +0700

    Session 10

---
+++ buoi11-God Tower/Assets/scripts/HintChanger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HintChanger : MonoBehaviour {
+//
+//	public Image hint1;
+//	public Image hint2;
+
+	public List<Image> hintList;
+	public Button hintButton;
+
+	int hintCurrentIndex = 0;
+
+	public void OnHintButtonClick (){
+		hintCurrentIndex = hintCurrentIndex + 1;
+
+		if (hintCurrentIndex == hintList.Count - 1) {
+			hintButton.transform.localScale = new Vector3 (-1, 1, 1);
+		} else {
+			hintButton.transform.localScale = new Vector3 (1,1,1);
+		}
+
+		if (hintCurrentIndex == hintList.Count ) {
+			hintCurrentIndex = 0;
+		}
+
+
+		for (int i = 0; i <= hintList.Count; i++) {
+			if (i == hintCurrentIndex) {
+				hintList [i].gameObject.SetActive (true);
+			}
+				else 
+					hintList[i].gameObject.SetActive(false);
+			}
+		}
+
+
+//	public void OnHintButtonClick (){
+//		if (hintCurrentIndex == 1) {
+//			hint1.gameObject.SetActive (false);
+//			hint2.gameObject.SetActive (true);
+//			hintCurrentIndex = 2;
+//		}
+//		else 
+//	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ buoi11-God Tower/Assets/scripts/level2/level2ahihi.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement ;
+
+public class level2ahihi : MonoBehaviour {
+	public Text levelText2;
+	public InputField inputField;
+	public Button submitButton;
+	public Text hintText;
+	public string levelContent2 = "LEVELS";
+	public string levelNumber2;
+	public string levelAnswer;
+
+	string answer ;
+
+	void Start () {
+		levelText2.text = levelContent2;
+		StartCoroutine (ChangeLevelTextRoutime ());
+
+	}
+
+	public void GetInput(){
+		answer = inputField.text;
+		CheckAnwer ();
+
+	}
+	public void CheckAnwer(){
+		if (answer == levelAnswer) {
+			hintText.text = "Yayyyyyyyyyyy";
+			SceneManager.LoadScene (3);
+			// TODO : Change Scence
+		}else {
+			hintText.text = "WRONGGGGGGGGG!!!!!!";
+			hintText.color = Color.red;
+			inputField.text = "";
+			inputField.ActivateInputField ();
+			// con tro se tro vao ngay o vua enter ma khong can dung con tro tro vao
+		}
+
+	}
+	// ham ne dung de : cu sau 1 khoang time thi se lap lai viec ne
+	IEnumerator ChangeLevelTextRoutime(){
+		while (true){
+			levelText2.text = levelContent2;
+			// Wait 2 seconds
+			yield return new WaitForSeconds (1f);
+			//Debug.Log ("????");
+			levelText2.text = levelNumber2;
+			yield return new WaitForSeconds (1f);
+		}
+	}
+	void Update () {
+
+	}
+}
