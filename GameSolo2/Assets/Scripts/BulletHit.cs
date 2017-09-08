24e6a9e87e5bdceae5906e59bab9e476987efe0a
PC\Admin <bxcau97@gmail.com>
2017-09-08 21:27:39 +0700

    soloGame

---
+++ GameSolo2/Assets/Scripts/BulletHit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHit : MonoBehaviour {
+
+	ProjectileController myPC;
+
+	public GameObject bulletExplosion;
+
+	void Awake (){
+		myPC = GetComponentInParent <ProjectileController>();
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "Shootable"){
+			myPC.removeForce ();
+			Instantiate (bulletExplosion, transform.position,transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+	void OnTrigerStay2D (Collider2D other){
+		if (other.gameObject.tag == "Shootable"){
+			myPC.removeForce ();
+			Instantiate (bulletExplosion, transform.position,transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ GameSolo2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed;
+	public float JumpHeight;
+
+	bool facingRight;
+	bool grounded;
+
+	// Kb cac bien de ban
+
+	public Transform GunTip;
+	public GameObject bullet;
+	float fireRate = 0.5f;
+	float nextFire = 0;
+
+	Rigidbody2D myBody;
+	Animator myAnim;
+		
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent <Rigidbody2D> ();
+		myAnim = GetComponent <Animator> ();
+
+		facingRight = true;
+	}
+
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+		myAnim.SetFloat ("speed", Mathf.Abs (move));
+
+		myBody.velocity = new Vector2 (move * maxSpeed, myBody.velocity.y);
+		if (move > 0 && !facingRight) {
+			flip ();
+		} else if (move < 0 && facingRight){
+			flip ();
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			if (grounded) {
+				grounded = false;
+				myBody.velocity = new Vector2 (myBody.velocity.x, JumpHeight);
+			}
+		}
+
+		// chuc nang ban duoc
+		if (Input.GetAxisRaw ("Fire1")>0) 
+			fireBullet ();
+	}
+	void flip (){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+
+	void OnCollisionEnter2D (Collision2D other){
+		if (other.gameObject.tag == "Ground") {
+			grounded = true;
+		}
+	}
+	//chuc nang ban
+	void fireBullet (){
+		if (Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			if (facingRight) {
+				Instantiate (bullet, GunTip.position, Quaternion.Euler (new Vector3 (0, 0, 0)));
+			}else if (!facingRight) {
+				Instantiate (bullet, GunTip.position, Quaternion.Euler (new Vector3 (0, 0, 180)));
+
+
+			}
+		}
+	}
+}
